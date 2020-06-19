65dd0e9e263c36e62a0d26187db77176b0c6fccd
Igor <iyakushin@thetalogin4.tmi.alcf.anl.gov>
2020-06-19 01:40:04 +0000

    env

--- summit_env.sh
+++ summit_env.sh
@@ -2,9 +2,9 @@ shopt -s direxpand
 module load python/3.7.0-anaconda3-5.3.0
 
 # This is specific to iyakushin account. 
-# Cheetah/Savanna is downloaded from github using "git clone https://github.com/CODARcode/cheetah.git"
-# Anaconda3 environment, Test10, is created with "conda create -n Test10"
+# Test10 environment is created with "conda create -n Test10"
 # Various packages like conda, pip, numpy, scipy, pandas, matplotlib is installed into the environment with "conda install ..."
+# Cheetah/Savanna is downloaded from github using "git clone https://github.com/CODARcode/cheetah.git"
 # Cheeta/Savanna is installed into the environment with "python setup.py install"
 
 
--- theta_env.sh
+++ theta_env.sh
@@ -2,10 +2,9 @@ module swap PrgEnv-intel/6.0.5 PrgEnv-gnu/6.0.5
 module load miniconda-3.6/conda-4.5.12
 
 # This is specific to iyakushin account. 
-# Cheetah/Savanna is downloaded from github using "git clone https://github.com/CODARcode/cheetah.git"
-# Anaconda3 environment, Test10, is created with "conda create -n Test10"
+# Test10 environment is created with "conda create -n Test10"
 # Various packages like conda, pip, numpy, scipy, pandas, matplotlib is installed into the environment with "conda install ..."
+# Cheetah/Savanna is downloaded from github using "git clone https://github.com/CODARcode/cheetah.git"
 # Cheeta/Savanna is installed into the environment with "python setup.py install"
-
 
 source activate Test10
