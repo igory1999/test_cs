f03845237eaed5556cd34b16548e6f5bf430ddd9
Igor <igor@pr7530>
2020-06-22 15:05:11 -0500

    2

--- 2/program/producer.py
+++ 2/program/producer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 #
 # Distributed under the OSI-approved Apache License, Version 2.0.  See
--- 2/setup.sh
+++ 2/setup.sh
@@ -1,2 +1,1 @@
-module load python/3.7.0-anaconda3-5.3.0
-source activate Test10
+export PATH=/home/igor/.conda/envs/T10/bin:$PATH
