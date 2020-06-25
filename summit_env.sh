f76bfc696be55682ea8a7b086ca2d9ca66dd8da3
Igor Yakushin <iyakushin@login3.summit.olcf.ornl.gov>
2020-06-25 17:31:27 -0400

    setup

--- summit_env.sh
+++ summit_env.sh
@@ -1,5 +1,16 @@
+module use $HOME/privatemodules
+export MYSOFT=$HOME/SOFT_INSTALL
+export MYSRC=$HOME/SOFT_SRC
+export MYMOD=$HOME/privatemodules
+export PATH=$HOME/bin:$PATH
+
+
 shopt -s direxpand
 module load python/3.7.0-anaconda3-5.3.0
+module load gcc/7.4.0
+module load Compiler/gcc/7.4.0/adios2/2.5_simplest-gcc-7.4.0
+module load zeromq/4.2.5
+module load libfabric/1.7.0
 
 # This is specific to iyakushin account. 
 # Test10 environment is created with "conda create -n Test10"
@@ -8,4 +19,4 @@ module load python/3.7.0-anaconda3-5.3.0
 # Cheeta/Savanna is installed into the environment with "python setup.py install"
 
 
-source activate Test10
+source activate Test11
